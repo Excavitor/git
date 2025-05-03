e195ce6c09a84f34e89f62f5aef3a1baa4e7df38
Shad Abdullah <shad2000abdullah@gmail.com>
2025-05-03 15:13:51 +0600

    multiplication added in calc branch_2

--- calc.py
+++ calc.py
@@ -7,8 +7,13 @@ def sum(first_number, second_number):
 def subtraction(first_number, second_number):
     return first_number - second_number
 
+def multiply(first_number, second_number):
+    return first_number * second_number
+
 sum_result = sum(first_number, second_number)
 subtraction_result = subtraction(first_number, second_number)
+multiply_result = multiply(first_number, second_number)
 
 print("Sum result:", sum_result)
 print("Subtraction result:", subtraction_result)
+print("Multiply result:", multiply_result)
