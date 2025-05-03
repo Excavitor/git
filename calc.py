ebc1e262fce78887ab04d4dcc928235e7ef616e9
Shad Abdullah <shad2000abdullah@gmail.com>
2025-05-03 15:07:14 +0600

    sub perform in calc branch 2

--- calc.py
+++ calc.py
@@ -4,5 +4,11 @@ second_number: int = int(input("Enter Second number: "))
 def sum(first_number, second_number):
     return first_number + second_number
 
+def subtraction(first_number, second_number):
+    return first_number - second_number
+
 sum_result = sum(first_number, second_number)
-print(sum_result)
+subtraction_result = subtraction(first_number, second_number)
+
+print("Sum result:", sum_result)
+print("Subtraction result:", subtraction_result)
