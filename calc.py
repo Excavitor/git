4a6e74381dbf07d17948a77ff2f705dcb28b9e36
Shad Abdullah <shad2000abdullah@gmail.com>
2025-05-03 15:03:46 +0600

    sum perform in calc

---
+++ calc.py
@@ -0,0 +1,8 @@
+first_number: int = int(input("Enter First number: "))
+second_number: int = int(input("Enter Second number: "))
+
+def sum(first_number, second_number):
+    return first_number + second_number
+
+sum_result = sum(first_number, second_number)
+print(sum_result)
