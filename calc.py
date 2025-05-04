50866b279c26c9f163fa231fdbfce13d8bec0574
Shad Abdullah <shad2000abdullah@gmail.com>
2025-05-04 15:10:41 +0600

    using squash, f and doc string added in sum function

--- calc.py
+++ calc.py
@@ -4,7 +4,8 @@ first_number: int = int(input("Enter First number: "))
 second_number: int = int(input("Enter Second number: "))
 
 def sum(first_number, second_number):
-    return first_number + second_number
+    """ This function calculates the sum of two numbers """
+    return f"Sum = {first_number + second_number}"
 
 def subtraction(first_number, second_number):
     if first_number > second_number:
